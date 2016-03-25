279bdcb372e0726dd3635882576d80d99d924a16
lunaroyster <oneinaquadrillion@live.com>
2016-03-25 10:16:28 +0530

    Started Post class

---
+++ poll/post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poll
+{
+    class Post
+    {
+        string name;
+        public string Name
+        {
+            get
+            {
+                return name != null ? name : "Unassigned";
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        class Candidate
+        {
+            string name;
+            public string Name
+            {
+                get
+                {
+                    return name != null ? name : "Unnamed";
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+        }
+    }
+}
