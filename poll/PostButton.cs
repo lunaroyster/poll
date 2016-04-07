1a09ba6fad2bfbed85d32e9fa051307eacc4c141
lunaroyster <oneinaquadrillion@live.com>
2016-04-07 13:03:46 +0530

    Added PostButton

---
+++ poll/PostButton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace poll
+{
+    
+    class PostButton : Button
+    {
+        private bool active;
+        public int PostID { get; set; }
+        public int CandidateID { get; set; }
+        private Brush activeBrush;
+        private Brush defaultBrush;
+        //public Post.Candidate LinkedCandidateObject { }
+
+        public PostButton()
+        {
+            defaultBrush = Background;
+        }
+
+        private void UpdateActivation()
+        {
+            if (active)
+            {
+                Background = activeBrush;
+            }
+            else
+            {
+                Background = defaultBrush;
+            }
+        }
+
+        public Brush ActiveBackground
+        {
+            get
+            {
+                return activeBrush;
+            }
+            set
+            {
+                activeBrush = value;
+            }
+        }
+        
+        public bool IsActive
+        {
+            get
+            {
+                return active;
+            }
+            set
+            {
+                active = value;
+                UpdateActivation();
+            }
+        }
+        
+    }
+}
